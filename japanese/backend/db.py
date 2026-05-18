4db8f1b14eac28439557aeaf656c7b746dbace51
ryanokimoto <ryan.okimoto@hotmail.com>
2026-05-18 15:00:48 -0700

    adding japanese tab

---
+++ japanese/backend/db.py
@@ -0,0 +1,12 @@
+# database initialization postgres
+
+
+"""
+video data 
+    - video title 
+    - video length 
+    - date watched (date added to playlist)
+    - channel
+    - video url
+    - 
+"""
